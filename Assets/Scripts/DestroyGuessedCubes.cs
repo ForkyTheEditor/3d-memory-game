cc8f5d61c09f9c24037152bc40c2dafb5da80fad
ForkyTheEditor <boohoochicken@gmail.com>
2017-02-14 20:31:56 +0200

    Add files via upload

--- Assets/Scripts/DestroyGuessedCubes.cs
+++ Assets/Scripts/DestroyGuessedCubes.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DestroyGuessedCubes : MonoBehaviour
 {
-   
+
     public GameObject destroyEffect;
 
     public void CheckCubes()
@@ -18,17 +18,20 @@ public class DestroyGuessedCubes : MonoBehaviour
                 turnedPlanes.Add(go);
             }
         }
-        if (turnedPlanes[0].GetComponent<Renderer>().material.mainTexture == turnedPlanes[1].GetComponent<Renderer>().material.mainTexture)
+        if (turnedPlanes.Count == 2)
         {
-            GameObject effect1 = Instantiate(destroyEffect, turnedPlanes[0].transform.position, turnedPlanes[0].transform.rotation);
-            Destroy(turnedPlanes[0].transform.parent.gameObject);
+            if (turnedPlanes[0].GetComponent<Renderer>().material.mainTexture == turnedPlanes[1].GetComponent<Renderer>().material.mainTexture)
+            {
+                GameObject effect1 = Instantiate(destroyEffect, turnedPlanes[0].transform.position, turnedPlanes[0].transform.rotation);
+                Destroy(turnedPlanes[0].transform.parent.gameObject);
 
-            GameObject effect2 = Instantiate(destroyEffect, turnedPlanes[1].transform.position, turnedPlanes[1].transform.rotation);
-            Destroy(turnedPlanes[1].transform.parent.gameObject);
+                GameObject effect2 = Instantiate(destroyEffect, turnedPlanes[1].transform.position, turnedPlanes[1].transform.rotation);
+                Destroy(turnedPlanes[1].transform.parent.gameObject);
 
-            Destroy(effect1, 2);
-            Destroy(effect2, 2);
-            ClickSquare.turnedSquares = 0;
+                Destroy(effect1, 2);
+                Destroy(effect2, 2);
+                ClickSquare.turnedSquares = 0;
+            }
         }
     }
 }
